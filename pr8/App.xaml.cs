2726aed21a4f062b0e044e0eb2900ac1fdea83e7
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2024-06-06 06:04:43 +0300

    Update App.xaml.cs

--- pr8/App.xaml.cs
+++ pr8/App.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.IO;
 
 namespace pr8
 {
@@ -9,6 +10,32 @@ namespace pr8
     /// </summary>
     public partial class App : Application
     {
+        private static string theme;
+        public static string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+                var dict = new ResourceDictionary { Source = new Uri($"/CustomLib;component/Themes/{value}.xaml", UriKind.Relative) };
+
+                Current.Resources.MergedDictionaries.RemoveAt(0);
+                Current.Resources.MergedDictionaries.Insert(0, dict);
+
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllText($"{desktop}\\theme.txt", value);
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (File.Exists($"{desktop}\\theme.txt"))
+            {
+                Theme = File.ReadAllText($"{desktop}\\theme.txt");
+            }
+        }
     }
 
 }
