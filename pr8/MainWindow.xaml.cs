8a8db3985dab050baf6c74dc5e03ef5f8bc9cd79
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2024-06-06 06:05:21 +0300

    Update MainWindow.xaml.cs

--- pr8/MainWindow.xaml.cs
+++ pr8/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using JsonLib;
+using Path = System.IO.Path;
+using Newtonsoft.Json;
+using System.IO;
+
 
 namespace pr8
 {
@@ -16,9 +13,72 @@ namespace pr8
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "BlueTheme";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "RedTheme";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string textToSerialize = TextBoxic.Text;
+                string jsonFilePath = saveFileDialog.FileName;
+
+         
+                File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(textToSerialize));
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string jsonFilePath = openFileDialog.FileName;
+                    string jsonText = File.ReadAllText(jsonFilePath);
+
+                    string txt = JsonConvert.DeserializeObject<string>(jsonText);
+
+                    if (!string.IsNullOrEmpty(txt))
+                    {
+                        TextBoxic.Text = txt;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл JSON пустой.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при чтении файла: {ex.Message}");
+                }
+            }
+
+        }
+
+
     }
 }
