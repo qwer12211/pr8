42fe702c2c68e049b073c2736aa0c2d140bbd07e
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2024-06-06 06:31:43 +0300

    Add files via upload

---
+++ JsonLib/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace JsonLib
+{
+    public class Json
+    {
+        public static void SerializeToFile<T>(T obj, string filePath)
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(obj);
+                File.WriteAllText(filePath, jsonString);
+                Console.WriteLine($"Объект успешно сериализован и записан в файл: {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка при записи файла: {ex.Message}");
+            }
+        }
+
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                T obj = JsonSerializer.Deserialize<T>(jsonString);
+                Console.WriteLine($"Объект успешно десериализован из файла: {filePath}");
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при десериализации: {ex.Message}");
+                return default;
+            }
+        }
+
+    }
+}
